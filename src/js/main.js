c07bb545ecfc50a993df9426464308319061bd44
Khoi Hoang <khoii87@gmail.com>
2023-10-15 21:37:06 +0200

    add: ✨ All heroes can attack and big boss is attacking back

--- src/js/main.js
+++ src/js/main.js
@@ -21,50 +21,52 @@ let healthRandomMonster = +lifeBarRandomMonster.textContent;
 let healthNamelessKnight = +lifeBarNamelessKnight.textContent;
 let healthJuliaTheArcher = +lifeBarJuliaTheArcher.textContent;
 
-let isComputerTurnBigBoss = false;
+const MIN_DMG = 5;
+const MAX_DMG = 10;
+
 let isComputerTurnRandomMonster = false;
 
-let isPlayerTurnTheCat = true;
-let isPlayerTurnJuliaTheArcher = true;
-let isPlayerTurnWilliamTheArcher = true;
-let isPlayerTurnTheNameLessKnight = true;
-let isPlayerTurnJackTheLumberJack = true;
-
-let isPlayerAllTurnedFinished = false;
 let isGameOver = false;
 
 let gameOutputMessage = '';
 
 const heroes = ['THE_CAT', 'NAMELESS_HERO', 'JULIA_THE_ARCHER'];
 
-playerTheCat.addEventListener('click', handleCatAttack);
+playerTheCat.addEventListener('click', handleAttackTheCat);
+playerNamelessKnight.addEventListener('click', handleAttackNamelessKnight);
+playerJuliaTheArcher.addEventListener('click', handleAttackJuliaTheArcher);
 
-function handleCatAttack(e) {
-	isPlayerTurnTheCat = false;
-
-	if (!isPlayerTurnTheCat) {
-		// e.preventDefault();
-		// playerTheCat.removeEventListener('click', handleCatAttack);
-	}
-
+function handleAttackTheCat() {
 	const catAttackDmg = generateAttackDamage();
 	healthBigBoss -= catAttackDmg;
 
 	regenerateLifeBarToDisplay(lifeBarBigBoss, healthBigBoss);
 
 	createOutputGameMessage(`The Cat gave damage ${catAttackDmg} to Big Boss`, 'cat-head');
-	attackBigBossOnRandomHero();
-
-	freezeTemporarilyPlayer(playerTheCat, isPlayerTurnTheCat);
+	attackRandomHeroByBigBoss();
 }
 
-function freezeTemporarilyPlayer(character, isTemporarilyFrozen = true) {
-	if (!isTemporarilyFrozen) {
-		character.classList.add('cursor-not-allowed', 'mix-blend-luminosity');
-	}
+function handleAttackNamelessKnight() {
+	const namelessKnightAttackDmg = generateAttackDamage();
+	healthBigBoss -= namelessKnightAttackDmg;
+
+	regenerateLifeBarToDisplay(lifeBarBigBoss, healthBigBoss);
+
+	createOutputGameMessage(`The Nameless Knight gave damage ${namelessKnightAttackDmg} to Big Boss`, 'knight');
+	attackRandomHeroByBigBoss();
 }
 
-function attackBigBossOnRandomHero() {
+function handleAttackJuliaTheArcher() {
+	const juliaTheArcherAttackDmg = generateAttackDamage();
+	healthBigBoss -= juliaTheArcherAttackDmg;
+
+	regenerateLifeBarToDisplay(lifeBarBigBoss, healthBigBoss);
+
+	createOutputGameMessage(`The Nameless Knight gave damage ${juliaTheArcherAttackDmg} to Big Boss`, 'knight');
+	attackRandomHeroByBigBoss();
+}
+
+function attackRandomHeroByBigBoss() {
 	const randomHeroes = getRandomHeroIndex(heroes);
 	const bigBossAttackDmg = generateAttackDamage();
 
@@ -74,28 +76,25 @@ function attackBigBossOnRandomHero() {
 		regenerateLifeBarToDisplay(lifeBarTheCat, healthTheCat);
 		createOutputGameMessage(`Big Boss attacked on The Cat ${bigBossAttackDmg}`, 'big-boss');
 	}
+
 	if (randomHeroes === 'NAMELESS_HERO') {
 		healthNamelessKnight -= bigBossAttackDmg;
 
 		regenerateLifeBarToDisplay(lifeBarNamelessKnight, healthNamelessKnight);
 		createOutputGameMessage(`Big Boss attacked on the Nameless knight ${bigBossAttackDmg}`, 'big-boss');
 	}
+
 	if (randomHeroes === 'JULIA_THE_ARCHER') {
 		healthJuliaTheArcher -= bigBossAttackDmg;
 
 		regenerateLifeBarToDisplay(lifeBarJuliaTheArcher, healthJuliaTheArcher);
 		createOutputGameMessage(`Big Boss attacked on Julia the Archer ${bigBossAttackDmg}`, 'big-boss');
 	}
-
-	console.log(randomHeroes);
 }
 
 function getRandomHeroIndex(arr) {
 	return arr[Math.floor(Math.random() * arr.length)];
 }
-
-const MIN_DMG = 5;
-const MAX_DMG = 10;
 
 function generateAttackDamage(min = MIN_DMG, max = MAX_DMG) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
